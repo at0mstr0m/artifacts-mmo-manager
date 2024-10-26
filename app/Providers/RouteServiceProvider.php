fb35b1db49dc543e455bc51ae7c5fd84e8d372b9
at0mstr0m <jan.u.hellwig@gmail.com>
2024-10-26 11:25:21 +0200

    add dev route for easier development

---
+++ app/Providers/RouteServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class RouteServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        $path = base_path('routes/dev.php');
+        if (env('APP_ENV') === 'local' && file_exists($path)) {
+            $this->loadRoutesFrom($path);
+        }
+    }
+}
