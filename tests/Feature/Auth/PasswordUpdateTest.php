c2d111d4ad086f3e801400de64e2fa1b1e0b46f0
at0mstr0m <jan.u.hellwig@gmail.com>
2024-10-27 21:34:10 +0100

    config ide-helper

--- tests/Feature/Auth/PasswordUpdateTest.php
+++ tests/Feature/Auth/PasswordUpdateTest.php
@@ -27,13 +27,11 @@ class PasswordUpdateTest extends TestCase
                 'current_password' => 'password',
                 'password' => 'new-password',
                 'password_confirmation' => 'new-password',
-            ])
-        ;
+            ]);
 
         $response
             ->assertSessionHasNoErrors()
-            ->assertRedirect('/profile')
-        ;
+            ->assertRedirect('/profile');
 
         $this->assertTrue(Hash::check('new-password', $user->refresh()->password));
     }
@@ -49,8 +47,7 @@ class PasswordUpdateTest extends TestCase
                 'current_password' => 'wrong-password',
                 'password' => 'new-password',
                 'password_confirmation' => 'new-password',
-            ])
-        ;
+            ]);
 
         $response
             ->assertSessionHasErrors('current_password')
