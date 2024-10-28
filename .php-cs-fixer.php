c0fb7c87498a9248aee6d482b5574f1f2baaa91a
at0mstr0m <jan.u.hellwig@gmail.com>
2024-10-28 22:14:41 +0100

    install + config friendsofphp/php-cs-fixer & spatie/laravel-data

---
+++ .php-cs-fixer.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+use PhpCsFixer\Config;
+use PhpCsFixer\Runner\Parallel\ParallelConfigFactory;
+
+return (new Config())
+    ->setParallelConfig(ParallelConfigFactory::detect())
+    ->setRules([
+        '@PSR12' => true,
+        '@PhpCsFixer' => true,
+        'single_quote' => true,
+        'binary_operator_spaces' => [
+            'operators' => [
+                '=>' => 'single_space',
+                '==' => 'single_space',
+            ],
+        ],
+        'concat_space' => ['spacing' => 'one'],
+        'phpdoc_align' => false,
+        'phpdoc_no_alias_tag' => false,
+        'phpdoc_separation' => true,
+        'phpdoc_to_comment' => [
+            'ignored_tags' => ['todo', 'var'],
+        ],
+        'space_after_semicolon' => true,
+        'trim_array_spaces' => true,
+        'yoda_style' => [
+            'equal' => false,
+            'identical' => false,
+            'less_and_greater' => false,
+        ],
+        'phpdoc_types_order' => [
+            'null_adjustment' => 'always_last',
+        ],
+        'declare_strict_types' => true,
+        'fully_qualified_strict_types' => [
+            'leading_backslash_in_global_namespace' => true,
+        ],
+        'phpdoc_order' => [
+            'order' => [
+                'property',
+                'property-read',
+                'property-write',
+                'method',
+                'param',
+                'return',
+                'throws',
+                'uses',
+                'mixin',
+            ],
+        ],
+        'string_implicit_backslashes' => [
+            'single_quoted' => 'ignore',
+        ],
+        'multiline_whitespace_before_semicolons' => true,
+    ])
+    ->setLineEnding("\n")
+;
