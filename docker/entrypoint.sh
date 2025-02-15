364d4a08717c723522139ea88f5e610162f7fd30
at0mstr0m <jan.u.hellwig@gmail.com>
2025-02-15 12:31:28 +0100

    enhance entrypoint script with informative echo statements and streamline cache clearing process

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,34 +1,39 @@
 #!/bin/sh
 
-# Set the base directory for the app
+echo "Set the base directory for the app"
 BASEDIR=/var/www/html
 
-# Check if the .env file exists
+echo "Check if the .env file exists"
 if [ ! -f "$BASEDIR/.env" ]; then
    # abort the script if the .env file does not exist
     echo "The .env file does not exist. Please create it."
     exit 1
 fi
 
-# Clear the old boostrap/cache
+echo "Clear the old boostrap/cache"
 php artisan clear-compiled
 
-# Clear the optimization cache
-php artisan optimize:clear
+# Clear the cache
+php artisan cache:clear
+php artisan event:clear
+php artisan config:clear
+php artisan route:clear
 
-# Generate application key
+echo "Generate application key"
 php artisan key:generate --force
 
-# Remove prior storage links that exist
+echo "Remove prior storage links that exist"
 rm -rf public/storage
 
-# Build up a new storage link
+echo "Build up a new storage link"
 php artisan storage:link
 
-# Set the permissions for the storage directories
-chown -R www-data:www-data "$BASEDIR"
-chmod -R 755 "$BASEDIR/storage"
+echo "Set the permissions for the storage directories"
+# chown -R www-data:www-data "$BASEDIR"
+# chmod -R 755 "$BASEDIR/storage"
+chmod -R a+w "$BASEDIR/storage"
 
+echo "run migrations"
 php artisan migrate --force
 
 # Check if running in production or development mode
@@ -38,5 +43,5 @@ else
     echo "Running in development mode"
 fi
 
-# Start supervisord
+echo "Starting supervisord"
 exec /usr/bin/supervisord -c /etc/supervisord.conf
