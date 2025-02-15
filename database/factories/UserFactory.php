320190547db1de05118a1f64f1f4054c03dd4d44
at0mstr0m <jan.u.hellwig@gmail.com>
2025-02-15 16:26:16 +0100

    refactor UserFactory and UserSeeder to use dynamic user data generation

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -26,8 +26,8 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => 'Max Mustermann',
-            'email' => 'max.mustermann@example.com',
+            'name' => fake()->name(),
+            'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
