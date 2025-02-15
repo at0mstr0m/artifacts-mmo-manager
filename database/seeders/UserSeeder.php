c4cb73f73d17f96a6b2823e4848bd92415887e33
at0mstr0m <jan.u.hellwig@gmail.com>
2025-02-15 16:05:16 +0100

    update UserFactory and UserSeeder to use fixed user data

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -14,7 +14,7 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory()->create([
+        User::factory()->firstOrCreate([
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
